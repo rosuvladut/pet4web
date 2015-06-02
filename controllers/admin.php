ca105089972475409f863a47fd6f6fb395940b9c
Rosu Vladut <rosuvladut@gmail.com>
2015-06-02 13:13:47 +0300

    csv export

--- controllers/admin.php
+++ controllers/admin.php
@@ -26,7 +26,102 @@ class Admin extends Controller{
                 $petpage = 1;
 
             //all petitions
+            $export=isset($_POST['exported'])?$_POST['exported']:NULL;
+            if($export) {
+//                var_dump($_POST);
+//                die();
+                //print_r($petmodel->orderByTitle()->find()->toCSV());
+                if(isset($_POST['exportorder'])) {
+                    switch($_POST['exportorder']){
+                        case "byname":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderByTitle()->find()->toCSV());
+                            break;
+                        case "bycatasc":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderByCategory()->find()->toCSV());
+                            break;
+                        case "bycatdesc":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderByCategory('desc')->find()->toCSV());
+                            break;
+                        case "bysignasc":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderBySigned()->find()->toCSV());
+                            break;
+                        case "bysigndesc":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderBySigned('desc')->find()->toCSV());
+                            break;
+                        case "bytargetdesc":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderByTarget('desc')->find()->toCSV());
+                            break;
+                        case "bytargetasc":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderByTarget()->find()->toCSV());
+                            break;
+                        case "bydateasc":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderByCreated()->find()->toCSV());
+                            break;
+                        case "bydatedesc":
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderByCreated('desc')->find()->toCSV());
+                            break;
+                        default:
+                            header('Content-Type: text/csv; charset=utf-8');
+                            header('Content-Disposition: attachment; filename=petitiondata.csv');
+                            print_r($petmodel->orderByCreated('desc')->find()->toCSV());
+                    }
+                }
+                die();
+            }
+            $petmodel = new \pet4web\PetitionsQuery();
             $petitionpages = $petmodel->paginate($petpage, 10);
+            if(isset($_GET['sort'])) {
+                switch($_GET['sort']){
+                    case "byname":
+                        $petitionpages=$petmodel->orderByTitle()->paginate($petpage,10);
+                        break;
+                    case "bycatasc":
+                        $petitionpetpages = $petmodel->orderByCategory()->paginate($petpage, 10);
+                        break;
+                    case "bycatdesc":
+                        $petitionpages = $petmodel->orderByCategory('desc')->paginate($petpage, 10);
+                        break;
+                    case "bysignasc":
+                        $petitionpages = $petmodel->orderBySigned()->paginate($petpage, 10);
+                        break;
+                    case "bysigndesc":
+                        $petitionpages = $petmodel->orderBySigned('desc')->paginate($petpage, 10);
+                        break;
+                    case "bytargetdesc":
+                        $petitionpages = $petmodel->orderByTarget('desc')->paginate($petpage, 10);
+                        break;
+                    case "bytargetasc":
+                        $petitionpages = $petmodel->orderByTarget()->paginate($petpage, 10);
+                        break;
+                    case "bydateasc":
+                        $petitionpages = $petmodel->orderByCreated()->paginate($petpage, 10);
+                        break;
+                    case "bydatedesc":
+                        $petitionpages = $petmodel->orderByCreated('desc')->paginate($petpage, 10);
+                        break;
+                }
+            }
+            else{
+                $petitionpages = $petmodel->orderByCreated('desc')->paginate($petpage, 10);
+            }
             if ($petitionpages->haveToPaginate()) {
                 $links = $petitionpages->getLinks();
                 $this->view->petpages = $links;
